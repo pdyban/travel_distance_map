54872412623cedc33fc891a0a51b5cce322ec923
Pavlo Dyban <pavlo.dyban@doctolib.com>
2020-06-10 22:58:19 +0200

    Disable API test if private key is not found

--- travel_distance_map/test/test_api.py
+++ travel_distance_map/test/test_api.py
@@ -21,51 +21,55 @@ class TestVBBAPICachedMock(unittest.TestCase):
         self.assertTrue(self.api.request(query))
 
 
-class TestVBBAPICached(unittest.TestCase):
-    def setUp(self):
-        cache = SQLiteCache(TEST_DB_FILE)
-        with open('ACCESS_ID.txt') as f:
-            ACCESS_ID = f.read().strip()
-        self.api = VBBAPICached(access_id=ACCESS_ID, cache=cache)
+if not os.path.exists('ACCESS_ID.txt'):
+    from warnings import warn
+    warn('TestVBBAPICached will not run, API key is missing')
+else:
+    class TestVBBAPICached(unittest.TestCase):
+        def setUp(self):
+            cache = SQLiteCache(TEST_DB_FILE)
+            with open('ACCESS_ID.txt') as f:
+                ACCESS_ID = f.read().strip()
+            self.api = VBBAPICached(access_id=ACCESS_ID, cache=cache)
 
-    def tearDown(self):
-        os.remove(TEST_DB_FILE)
+        def tearDown(self):
+            os.remove(TEST_DB_FILE)
 
-    def test_get_closest_stop(self):
-        location = GPSPoint(52.5219216,13.411026)  # Berlin, Alexanderplatz
-        result = self.api.get_closest_stop(location)
-        self.assertEqual('S+U Alexanderplatz Bhf/Dircksenstr. (Berlin)', result.name)
-        self.assertEqual('900100024', result.id)
-        # should now pull result from cache and the result should be in same format as the original
-        result2 = self.api.get_closest_stop(location)
-        self.assertEqual(result2, result)
-        # should be able to operate both on GPSPoint and Position objects
-        position = Position(52.5219216,13.411026,'0','Alexanderplatz')
-        result3 = self.api.get_closest_stop(position)
-        self.assertEqual(result3, result)
+        def test_get_closest_stop(self):
+            location = GPSPoint(52.5219216,13.411026)  # Berlin, Alexanderplatz
+            result = self.api.get_closest_stop(location)
+            self.assertEqual('S+U Alexanderplatz Bhf/Dircksenstr. (Berlin)', result.name)
+            self.assertEqual('900100024', result.id)
+            # should now pull result from cache and the result should be in same format as the original
+            result2 = self.api.get_closest_stop(location)
+            self.assertEqual(result2, result)
+            # should be able to operate both on GPSPoint and Position objects
+            position = Position(52.5219216,13.411026,'0','Alexanderplatz')
+            result3 = self.api.get_closest_stop(position)
+            self.assertEqual(result3, result)
 
-    def test_can_init_old_cache(self):
-        cache = SQLiteCache(TEST_DB_FILE)
-        with open('ACCESS_ID.txt') as f:
-            ACCESS_ID = f.read().strip()
-        api2 = VBBAPICached(access_id=ACCESS_ID, cache=cache)
-        result = api2.get_closest_stop(GPSPoint(52.5219216,13.411026))
-        self.assertEqual('900100024', result.id)
+        def test_can_init_old_cache(self):
+            cache = SQLiteCache(TEST_DB_FILE)
+            with open('ACCESS_ID.txt') as f:
+                ACCESS_ID = f.read().strip()
+            api2 = VBBAPICached(access_id=ACCESS_ID, cache=cache)
+            result = api2.get_closest_stop(GPSPoint(52.5219216,13.411026))
+            self.assertEqual('900100024', result.id)
 
-    def test_get_all_trips(self):
-        start = Position(52.5219216, 13.411026, 900100003, 'Alexanderplatz')  # Alexanderplatz
-        end = Position(52.5201169, 13.3865786, 900100001, 'Friedrichstrasse')  # Friedrichstrasse
-        result = self.api.get_all_trips(start, end)
-        self.assertIsNotNone(result)
-        self.assertIsInstance(result, list)
+        def test_get_all_trips(self):
+            start = Position(52.5219216, 13.411026, 900100003, 'Alexanderplatz')  # Alexanderplatz
+            end = Position(52.5201169, 13.3865786, 900100001, 'Friedrichstrasse')  # Friedrichstrasse
+            result = self.api.get_all_trips(start, end)
+            self.assertIsNotNone(result)
+            self.assertIsInstance(result, list)
 
-    def test_get_all_trips_fixed_datetime(self):
-        start = Position(52.5219216, 13.411026, 900100003, 'Alexanderplatz')  # Alexanderplatz
-        end = Position(52.5201169, 13.3865786, 900100001, 'Friedrichstrasse')  # Friedrichstrasse
-        time = datetime(2020,10,1,12,0,0)
-        result = self.api.get_all_trips(start, end, datetime_=time)
-        self.assertIsNotNone(result)
-        self.assertIsInstance(result, list)
+        def test_get_all_trips_fixed_datetime(self):
+            start = Position(52.5219216, 13.411026, 900100003, 'Alexanderplatz')  # Alexanderplatz
+            end = Position(52.5201169, 13.3865786, 900100001, 'Friedrichstrasse')  # Friedrichstrasse
+            time = datetime(2020,10,1,12,0,0)
+            result = self.api.get_all_trips(start, end, datetime_=time)
+            self.assertIsNotNone(result)
+            self.assertIsInstance(result, list)
 
 
 if __name__ == '__main__':
